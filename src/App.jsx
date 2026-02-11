b5255aeb288caa316024a064eea90f686f318c34
dhvanikoshti <dhvanikoshti26@gmail.com>
2026-02-11 12:52:09 +0530

    Move File

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './home'
-import Task1 from './Task1.jsx'
+import Task1 from './Task1/Task1.jsx'
 
 function App() {
    
