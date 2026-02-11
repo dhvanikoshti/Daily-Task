d8e3593a23f0e757f11910bd8057daf8eb1a822f
dhvanikoshti <dhvanikoshti26@gmail.com>
2026-02-11 10:20:52 +0530

    Initial Commit

--- src/Task1.jsx
+++ src/Task1.jsx
@@ -4,14 +4,14 @@ import Task1_Component from './Task1_Component'
 function Task1() {
   return (
     <div className=' m-10 md:m-20 border-2 shadow-md border-gray-400 rounded-2xl md:rounded-4xl bg-gray-200'>
-        <div className='text-2xl sm:text-3xl md:text-4xl font-bold text-center  m-9  font-serif'>
+        <div className='text-2xl tracking-wide sm:text-3xl md:text-4xl font-bold text-center  m-9  font-serif'>
             Departments
         </div>
         <div className='m-6'>
         <hr className=' border-2 border-gray-600'/>
         </div>
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 m-10'>
-            <Task1_Component heading="Computer Engineering" content="Computer Engineering is a four year Ungraduate Program introducing its students to realms of computer,introducing the theory and design of Data storage,software,operating system."/>
+            <Task1_Component heading="Computer Engineering" content="Computer Engineering is a four year Ungraduate Program introducing its students to realms of computer, introducing the theory and design of Data storage,software, operating system."/>
             <Task1_Component heading="IT Engineering" content="Information Technology is an undergraduate engineering course which focuses on expanding and growing in terms of dissemination of knowledge within and outside curriculum and skill development activities."/>
             <Task1_Component heading="Computer Science" content="Computer Engineering with ML & AI at Silver Oak University is a 4-year undergraduate specialization programme that presents a solid foundation in the principles and technologies to get on the path of an exciting, sprouting career."/>
         </div>
--- src/Task1_Component.jsx
+++ src/Task1_Component.jsx
@@ -6,7 +6,7 @@ function Task1_Component(props){
     return(
         <>
         <div className="border-2 border-gray-400 bg-white rounded-4xl shadow-md p-8 text-center flex flex-col justify-between">
-            <div className='font-extrabold text-lg sm:text-xl  text-center'>{props.heading}</div>
+            <div className='font-bold text-lg sm:text-xl  text-center'>{props.heading}</div>
             <div className='mt-2 text-sm  sm:text-base  text-center'>{props.content}</div>
             <div className='mt-4'>
                 <button className='hover:bg-gray-400 hover:border-gray-600 bg-gray-300 p-2 pl-4 pr-4 rounded-2xl border-2 border-gray-400'>Apply Now</button>
